f2c7020fc4fd873d7b2e83db74d2802cc7697cdf
Amadea <53259552+amadea-system@users.noreply.github.com>
2021-01-15 11:46:14 -0500

    Cleanup code and keymaps

--- keyboards/lily58/keymaps/amadea-system/keymap.c
+++ keyboards/lily58/keymaps/amadea-system/keymap.c
@@ -92,23 +92,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI \
 ),
 */
-
+#define OSM_LSFT OSM(MOD_LSFT)
  [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-  OSM(MOD_LSFT),  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LEAD, KC_RBRC,  KC_N,    KC_M,    TD(TD_COMMA), TD(TD_DOT),  KC_SLSH,  KC_RSFT, \
-                        KC_LALT, OSM(MOD_LSFT), MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI \
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
+  OSM_LSFT, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LEAD,    KC_RBRC,  KC_N,    KC_M,    TD(TD_COMMA), TD(TD_DOT),  KC_SLSH,  KC_RSFT, \
+                        KC_LALT, OSM_LSFT, MO(_LOWER), KC_SPC,    KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI \
 ),
+
 /* LOWER - Acessed by pressing 'Lower' Key
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |  F12 |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |      |      |Pg Up |  Up  |Pg Dwn|   {  |                    |      |   }  |      |      |      |   =  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
+ * |      |   !  | Left | Down |Right |   %  |-------.    ,-------|   ^  |   &  |   *  |      |   |  |      |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
+ * |      |      |      |      |      |      |-------|    |-------|      |   _  |   [  |   ]  |   \  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -128,8 +129,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
   KC_NO,   KC_NO,   KC_PGUP, KC_UP,   KC_PGDN, KC_LCBR,                   KC_NO,   KC_RCBR, KC_NO,   KC_NO,   KC_NO,   KC_EQL,  \
-  _______,  KC_EXLM, KC_LEFT, KC_DOWN, KC_RGHT, KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_NO,   KC_PIPE, KC_NO,   \
-  _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS, \
+  _______, KC_EXLM, KC_LEFT, KC_DOWN, KC_RGHT, KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_NO,   KC_PIPE, KC_NO,   \
+  _______, _______, _______, _______, _______, _______, KC_RBRC, _______, XXXXXXX, KC_UNDS, KC_LBRC, KC_RBRC, KC_BSLS, _______, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
 
@@ -139,13 +140,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //       KC_TRNS, KC_TRNS, KC_TRNS,                                KC_TRNS, KC_TRNS,     MO(3), KC_DEL, KC_TRNS
 // ),
 
+
 /* RAISE - Acessed by pressing 'Raise' Key
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |  🌙  |  🦋 |  😺 |      |      |                    |      |      |      |      |      |      |
+ * |      |  🌙  |  🦋 |  😺 |      |      |                    |      |      |      |      |      |Vol Up|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
+ * |      |      |      |      |      |      |                    |      |      |GUI Up|      |      |Vol Dw|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------|      | Left | Down |  Up  |Right |      |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------|      |GUI Lf|GUI Dw|GUI Rt|      | Mute |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -194,13 +196,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 
+#define TOG_ESDF TG(_GAME_ESDF)
+#define TOG_WASD TG(_GAME_WASD)
 
 [_ADJUST] = LAYOUT( \
-VRSN,    XXXXXXX, TG(_GAME),  TG(_GAME_ESDF), XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,   \
-XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,        XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,        XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-                              _______,        _______, _______, _______, _______,  _______, _______, _______ \
+VRSN,    XXXXXXX, TOG_WASD, TOG_ESDF, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,   \
+XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
+XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, \
+                            _______,  _______, _______, _______, _______,  _______, _______, _______ \
 ),
 
 
@@ -219,12 +223,12 @@ XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
  *                   `----------------------------'           '------''--------------------'
  */
 
-[_GAME] = LAYOUT( \
-KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-KC_LSFT, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LEAD, KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                      KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_ENT, TG(_GAME), KC_BSPC, KC_RGUI \
+[_GAME_WASD] = LAYOUT( \
+KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
+KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
+KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LEAD, KC_SPC,  KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+                           KC_LALT, KC_SPC, KC_SPC, KC_SPC,  KC_ENT,  TOG_WASD, KC_BSPC, KC_RGUI \
 ),
 
 
@@ -249,8 +253,8 @@ KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LEAD, KC_SPC,  KC_N,    
 KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
 KC_TAB,   KC_Q,   KC_E,    KC_W,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
 KC_G,  KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-KC_Z, KC_LCTRL,   KC_X,    KC_C,    KC_V,    KC_B,   KC_SPC,   KC_SPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                         KC_LALT, KC_SPC, KC_SPC, KC_SPC,        KC_ENT, TG(_GAME_ESDF), KC_BSPC, KC_RGUI \
+KC_Z, KC_LCTRL,   KC_X,    KC_C,    KC_V,    KC_B, KC_SPC,     KC_SPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+                          KC_LALT, KC_SPC, KC_SPC, KC_SPC,     KC_ENT, TOG_ESDF,KC_BSPC, KC_RGUI \
 )
 
 
@@ -304,11 +308,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 //SSD1306 OLED update loop, make sure to enable OLED_DRIVER_ENABLE=yes in rules.mk
 #ifdef OLED_DRIVER_ENABLE
 
-// oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-//   if (!is_keyboard_master())
-//     return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-//   return rotation;
-// }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   if (!is_keyboard_master())
@@ -368,36 +367,6 @@ void oled_task_user(void) {
   }
 }
 
-// void oled_task_user(void) {
-//   if (is_keyboard_master()) {
-//     // If you want to change the display of OLED, you need to change here
-
-//     // Show keyboard layout
-//     // oled_write(read_layer_state(), false);
-//     render_default_layer_state();
-
-//     // Add a empty line
-//     oled_write_ln_P(PSTR("\n-----"), false);
-//     // Show Keylogger
-//     // oled_write_ln(read_keylog(), false);
-//     // oled_write_ln(read_keylogs(), false);
-
-//     // Add a empty line
-//     // oled_write_ln_P(PSTR("-----"), false);
-
-//     // Show modifier status
-//     render_mod_status(get_mods());
-
-//     // oled_write_ln_P(PSTR(""), false);
-//     // oled_write_ln_P(PSTR(""), false);
-//     // oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-
-//     // oled_write_ln(read_host_led_state(), false);
-//     // oled_write_ln(read_timelog(), false);
-//   } else {
-//     oled_write(read_logo(), false);
-//   }
-// }
 #endif // OLED_DRIVER_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
