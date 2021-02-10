cb2f6b7157f2297fef68f5c9e6ed594b9cb11d81
Amadea <53259552+amadea-system@users.noreply.github.com>
2021-02-10 10:22:06 -0500

    [Lily58] Removed -/= Tap Dance

--- keyboards/lily58/keymaps/amadea-system/keymap.c
+++ keyboards/lily58/keymaps/amadea-system/keymap.c
@@ -126,14 +126,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 */
 #define OSM_LSFT OSM(MOD_LSFT)
-#define TD_MINS  TD_MINUS_EQL      // Send '-' (Minus Key) on a Tap, '=' (Equal Key) on a Double Tap, 
+// #define TD_MINS  TD(TD_MINUS_EQL)      // Send '-' (Minus Key) on a Tap, '=' (Equal Key) on a Double Tap, 
 // #define MT_LEAD  HYPR_T(KC_LEAD)   // Left Control, Shift, Alt and GUI when held, 'Leader Key' when tapped 
 
  [_QWERTY] = LAYOUT( \
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(TD_MINS), \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   OSM_LSFT, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LEAD,    KC_HYPR,  KC_N,    KC_M,    KC_COMMA, KC_DOT, KC_SLSH, KC_EQL, \
+  KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LEAD,   KC_HYPR,   KC_N,    KC_M,    KC_COMMA, KC_DOT, KC_SLSH, KC_EQL, \
                         KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,    KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI \
 ),
 
@@ -203,11 +203,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
 [_RAISE] = LAYOUT( \
-  XXXXXXX, E_INTRO,  E_MOON,  E_BFLY,  E_S_CAT, XXXXXXX,                     XXXXXXX, XXXXXXX,       XXXXXXX,       XXXXXXX,       XXXXXXX, KC_AUDIO_VOL_UP, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX,       LGUI(KC_UP),   XXXXXXX,       XXXXXXX, KC_AUDIO_VOL_DOWN, \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       XXXXXXX, LGUI(KC_LEFT), LGUI(KC_DOWN), LGUI(KC_RGHT), XXXXXXX, KC_AUDIO_MUTE, \
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS,       KC_EQL,        KC_LBRC,       KC_RBRC, KC_BSLS, \
-                             _______, _______, _______,  _______, _______,  _______, KC_DEL, _______ \
+  XXXXXXX,  E_INTRO,  E_MOON,  E_BFLY,  E_S_CAT, XXXXXXX,                     XXXXXXX, XXXXXXX,       XXXXXXX,       XXXXXXX,       XXXXXXX,  KC_AUDIO_VOL_UP, \
+  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX,       LGUI(KC_UP),   XXXXXXX,       XXXXXXX,  KC_AUDIO_VOL_DOWN, \
+  KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       XXXXXXX, LGUI(KC_LEFT), LGUI(KC_DOWN), LGUI(KC_RGHT), XXXXXXX,  KC_AUDIO_MUTE, \
+  KC_F7,    KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS,       KC_EQL,        KC_LBRC,       KC_RBRC,  KC_BSLS, \
+                               _______, _______, _______,  _______, _______,  _______, KC_DEL,        _______ \
 ),
 
 // [2] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,                      KC_NO,   KC_NO,         KC_NO,         KC_NO,         KC_NO,   KC_VOLU, 
